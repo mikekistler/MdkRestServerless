87c4ce06c0b822215a2f6a2b22ced9b0e54e501e
Mike Kistler <mikekistler@microsoft.com>
2024-04-15 17:25:21 -0500

    Add packages and Database layer classes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .Build();
+var builder = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults();
+
+builder.ConfigureServices(services =>
+{
+    var keyVaultUrl = new Uri(Environment.GetEnvironmentVariable("keyVaultUrl") ?? "");
+    var secretClient = new SecretClient(keyVaultUrl, new DefaultAzureCredential());
+    var connectionString = secretClient.GetSecret("ConnectionStrings.ProductsDb").Value.Value;
+    services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+});
+
+var host = builder.Build();
 
 host.Run();
