6026a2652d8115e8bff0cee06ba0bb49d948fb65
Mike Kistler <mikekistler@microsoft.com>
2024-04-16 14:13:55 -0500

    Add FunctionApps for Products and ProductItem

---
+++ ProductItemEndpoint.cs
@@ -0,0 +1,81 @@
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace MdkRestServerless
+{
+    public class ProductItemEndpoint
+    {
+        private readonly ILogger _logger;
+        private readonly AppDbContext _dbContext;
+
+        public ProductItemEndpoint(ILoggerFactory loggerFactory, AppDbContext dbContext)
+        {
+            _logger = loggerFactory.CreateLogger<ProductItemEndpoint>();
+            _dbContext = dbContext;
+        }
+
+        [Function("ProductItemEndpoint")]
+        public async Task<HttpResponseData> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "patch", "delete", Route = "products/{id}")] HttpRequestData req, int id)
+        {
+            _logger.LogInformation($"ProductItemEndpoint function processed a ${req.Method} request.");
+
+            if (req.Method == "GET")
+            {
+                var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+                if (product == null)
+                {
+                    return req.CreateResponse(HttpStatusCode.NotFound);
+                }
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+                await response.WriteAsJsonAsync(product);
+
+                return response;
+            }
+            if (req.Method == "PATCH")
+            {
+                var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+                if (product == null)
+                {
+                    return req.CreateResponse(HttpStatusCode.NotFound);
+                }
+                // deserialize the request body into a Product object with System.Text.Json
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var productPatch = JsonSerializer.Deserialize<JsonObject>(requestBody);
+                if (productPatch == null)
+                {
+                    _logger.LogError("Invalid product data.");
+                    return req.CreateResponse(HttpStatusCode.BadRequest);
+                }
+                // update the product with the patch data
+                await _dbContext.SaveChangesAsync();
+
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+                await response.WriteAsJsonAsync(product);
+
+                return response;
+            }
+            if (req.Method == "DELETE")
+            {
+                var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+                if (product == null)
+                {
+                    return req.CreateResponse(HttpStatusCode.NotFound);
+                }
+                _dbContext.Products.Remove(product);
+                await _dbContext.SaveChangesAsync();
+
+                return req.CreateResponse(HttpStatusCode.NoContent);
+            }
+
+            return req.CreateResponse(HttpStatusCode.MethodNotAllowed);
+        }
+    }
+}
---
+++ ProductsEndpoint.cs
@@ -0,0 +1,55 @@
+using System.Net;
+using System.Text.Json;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace MdkRestServerless;
+
+public class ProductsEndpoint
+{
+    private readonly ILogger _logger;
+    private readonly AppDbContext _dbContext;
+
+    public ProductsEndpoint(ILoggerFactory loggerFactory, AppDbContext dbContext)
+    {
+        _logger = loggerFactory.CreateLogger<ProductsEndpoint>();
+        _dbContext = dbContext;
+    }
+
+    [Function("ProductsEndpoint")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.User, "get", "post", Route ="products")] HttpRequestData req)
+    {
+        _logger.LogInformation($"ProductsEndpoint function processed a ${req.Method} request.");
+
+        if (req.Method == "POST")
+        {
+            // deserialize the request body into a Product object with System.Text.Json
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var product = JsonSerializer.Deserialize<Product>(requestBody);
+            if (product == null)
+            {
+                _logger.LogError("Invalid product data.");
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            _dbContext.Products.Add(product);
+            await _dbContext.SaveChangesAsync();
+
+            var response = req.CreateResponse(HttpStatusCode.Created);
+            response.Headers.Add("Location", $"/products/{product.Id}");
+            return response;
+        }
+        if (req.Method == "GET")
+        {
+            var products = await _dbContext.Products.ToArrayAsync<Product>();
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            await response.WriteAsJsonAsync(products);
+            return response;
+        }
+
+        return req.CreateResponse(HttpStatusCode.MethodNotAllowed);
+    }
+}
